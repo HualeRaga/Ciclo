e02fc6040eb36c49bd1a18d03d8e60534ae34e8b
Hugo Alejandro Ramos Garcia <hramos0@ucol.mx>
2019-08-24 16:32:19 -0500

    Ciclos

---
+++ Ciclo/Ciclo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Tarea_For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            Console.WriteLine("Escribe hasta donde arrancamos el ciclo: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escribe hasta donde terminamos el ciclo el ciclo:");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("De cuanto en cuanto? ");
+            z = Convert.ToInt32(Console.ReadLine());
+
+            if (x < y)
+            {
+                for (int i = x; i <= y; i += z)
+                {
+                    Console.WriteLine("i = " + i);
+                }
+            }
+            if (x > y)
+            {
+                for (int i = x; i >= y; i -= z)
+                {
+                    Console.WriteLine("i = " + i);
+                }
+            }
+        }
+    }
+}
